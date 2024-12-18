c9b571c7949a7dfee88c63752da1163a75f7c486
LizAraLeonGarcia <lizbeth.lgarcia@alumnos.udg.mx>
2024-12-18 15:33:24 -0600

    listado-de-entregas colores a la primera tercera parte

--- resources/views/listado-de-entregas.blade.php
+++ resources/views/listado-de-entregas.blade.php
@@ -22,7 +22,7 @@
       <!-- ---------------------------------------------------- Columna central (tabla) ---------------------------------------------------- -->
       <div class="col-md-6">
         <div class="table-responsive">
-          <table class="table table-bordered table-striped table-hover table-sm shadow-lg">
+          <table class="table table-bordered table-striped table-sm">
             <thead class="thead-dark">
               <tr>
                 <th>#</th>
@@ -32,151 +32,151 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
+              <tr class="fila1">
                 <td>1</td>
                 <td>Ryu</td>
                 <td>27 septiembre 2022</td>
                 <td>$50</td>
               </tr>
-              <tr>
+              <tr class="fila2">
                 <td>2</td>
                 <td>Ken</td>
                 <td>11 octubre 2022</td>
                 <td>$180</td>
               </tr>
-              <tr>
+              <tr class="fila3">
                 <td>3</td>
                 <td>M. Bison</td>
                 <td>25 octubre 2022</td>
                 <td>$300</td>
               </tr>
-              <tr>
+              <tr class="fila4">
                 <td>4</td>
                 <td>Sagat</td>
                 <td>8 noviembre 2022</td>
                 <td>$300</td>
               </tr>
-              <tr>
+              <tr class="fila5">
                 <td>5</td>
                 <td>Blanka</td>
                 <td>22 noviembre 2022</td>
                 <td>$300</td>
               </tr>
-              <tr>
+              <tr class="fila6">
                 <td>6</td>
                 <td>Vega</td>
                 <td>6 diciembre 2022</td>
                 <td>$300</td>
               </tr>
-              <tr>
+              <tr class="fila7">
                 <td>7</td>
                 <td>Chun Li</td>
                 <td>20 diciembre 2022</td>
                 <td>$300</td>
               </tr>
-              <tr>
+              <tr class="fila8">
                 <td>8</td>
                 <td>Guile</td>
                 <td>10 enero 2023</td>
                 <td>$300</td>
               </tr>
-              <tr>
+              <tr class="fila9">
                 <td>9</td>
                 <td>Dhalsim</td>
                 <td>7 febrero 2023</td>
                 <td>$330</td>
               </tr>
-              <tr>
+              <tr class="fila10">
                 <td>10</td>
                 <td>Zangief</td>
                 <td>21 febrero 2023</td>
                 <td>$330</td>
               </tr>
-              <tr>
+              <tr class="fila11">
                 <td>11</td>
                 <td>Balrog</td>
                 <td>7 marzo 2023</td>
                 <td>$330</td>
               </tr>
-              <tr>
+              <tr class="fila12">
                 <td>12</td>
                 <td>E. Honda</td>
                 <td>21 marzo 2023</td>
                 <td>$330</td>
               </tr>
-              <tr>
+              <tr class="fila13">
                 <td>13</td>
                 <td>T. Hawk</td>
                 <td>18 abril 2023</td>
                 <td>$330</td>
               </tr>
-              <tr>
+              <tr class="fila14">
                 <td>14</td>
                 <td>Cammy</td>
                 <td>2 mayo 2023</td>
                 <td>$330</td>
               </tr>
-              <tr>
+              <tr class="fila15">
                 <td>15</td>
                 <td>Fei Long</td>
                 <td>16 mayo 2023</td>
                 <td>$330</td>
               </tr>
-              <tr>
+              <tr class="fila16">
                 <td>16</td>
                 <td>Akuma</td>
                 <td>30 mayo 2023</td>
                 <td>$330</td>
               </tr>
-              <tr>
+              <tr class="fila17">
                 <td>17</td>
                 <td>Sakura</td>
                 <td>27 junio 2023</td>
                 <td>$330</td>
               </tr>
-              <tr>
+              <tr class="fila18">
                 <td>18</td>
                 <td>Dee Jay</td>
                 <td>25 julio 2023</td>
                 <td>$360</td>
               </tr>
-              <tr>
+              <tr class="fila19">
                 <td>19</td>
                 <td>Adon</td>
                 <td>11 julio 2023</td>
                 <td>$360</td>
               </tr>
-              <tr>
+              <tr class="fila20">
                 <td>20</td>
                 <td>Rose</td>
                 <td>13 junio 2023</td>
                 <td>$330</td>
               </tr>
-              <tr>
+              <tr class="fila21">
                 <td>21</td>
                 <td>Dan</td>
                 <td>12 diciembre 2023</td>
                 <td>$360</td>
               </tr>
-              <tr>
+              <tr class="fila22">
                 <td>22</td>
                 <td>R. Mika</td>
                 <td>22 agosto 2023</td>
                 <td>$360</td>
               </tr>
-              <tr>
+              <tr class="fila23">
                 <td>23</td>
                 <td>Makoto</td>
                 <td>31 octubre 2023</td>
                 <td>$360</td>
               </tr>
-              <tr>
+              <tr class="fila24">
                 <td>24</td>
                 <td>Cody</td>
                 <td>3 octubre 2023</td>
                 <td>$360</td>
               </tr>
-              <tr>
+              <tr class="fila25">
                 <td>25</td>
                 <td>Ibuki</td>
                 <td>5 septiembre 2023</td>
