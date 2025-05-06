59657c48d331460a8978aa2c1d85cb4bcebf5333
LizAraLeonGarcia <lizbeth.lgarcia@alumnos.udg.mx>
2025-05-06 14:13:04 -0600

    responsividad en Navbar lista

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,12 +3,12 @@
         <img src="{{ asset('assets/img/navbar/1.jpg') }}" alt="Chun Li Alpha" class="img-fluid">
     </div>
     <ul class="nav-list">
-        <li class="nav-item"><a href="{{ url('/') }}" class="nav-link">Inicio</a></li>
-        <li class="nav-item"><a href="{{ url('/listado-de-entregas') }}" class="nav-link">Listado de entregas</a></li>
-        <li class="nav-item"><a href="{{ url('/personajes') }}" class="nav-link">Personajes</a></li>
-        <li class="nav-item"><a href="{{ url('/figuras') }}" class="nav-link">Figuras</a></li>
-        <li class="nav-item"><a href="{{ url('/revistas') }}" class="nav-link">Revistas</a></li>
-        <li class="nav-item"><a href="{{ url('/galeria') }}" class="nav-link">Galería</a></li>
+        <li class="nav-item"><a href="{{ url('/') }}" class="nav-link {{ Request::is('/') ? 'active' : '' }}">Inicio</a></li>
+        <li class="nav-item"><a href="{{ url('/listado-de-entregas') }}" class="nav-link {{ Request::is('listado-de-entregas') ? 'active' : '' }}">Listado de entregas</a></li>
+        <li class="nav-item"><a href="{{ url('/personajes') }}" class="nav-link {{ Request::is('personajes') ? 'active' : '' }}">Personajes</a></li>
+        <li class="nav-item"><a href="{{ url('/figuras') }}" class="nav-link {{ Request::is('figuras') ? 'active' : '' }}">Figuras</a></li>
+        <li class="nav-item"><a href="{{ url('/revistas') }}" class="nav-link {{ Request::is('revistas') ? 'active' : '' }}">Revistas</a></li>
+        <li class="nav-item"><a href="{{ url('/galeria') }}" class="nav-link {{ Request::is('galeria') ? 'active' : '' }}">Galería</a></li>
     </ul>
     <div class="navbarIMGderecha">
         <img src="{{ asset('assets/img/navbar/2.png') }}" alt="Chun Li Clásico" class="img-fluid">
